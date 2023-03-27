44a88aa25d0353fd8cc7e6538cb779a1310a6ec1
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2023-03-27 07:49:06 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,1 +1,23 @@
 """Laczenie z baza danych i dodanie ich"""
+"""Laczenie z baza danych i dodanie ich"""
+import mysql.connector
+from main import App
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="systemLogowania"
+)
+
+mycursor = mydb.cursor()
+def add(imie:str,nazwisko:str,wiek:int,miesiac:str,pesel = None):
+    sql = "INSERT INTO uzytownik (`imie`, `nazwisko`, `imie`, `nazwisko`, `wiek`, `miesiac`, `pesel` ) VALUES (%s, %s)"
+    val = (imie,nazwisko,wiek,miesiac,pesel if pesel is not None else "NULL")
+    mycursor.execute(sql, val)
+
+mycursor.execute("SELECT * FROM uzytkownik")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
